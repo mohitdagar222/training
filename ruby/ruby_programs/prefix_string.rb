0e47b3d50cbd9526297e822fec57cb65cecdcd92
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-17 17:13:00 +0530

    create program to a string is prefix of words array or not

---
+++ ruby/ruby_programs/prefix_string.rb
@@ -0,0 +1,23 @@
+=begin
+Given a string s and an array of strings words, determine whether s is a prefix string of words. 
+A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some 
+positive k no larger than words.length. Return true if s is a prefix string of words, or false otherwise
+=end
+
+class Test
+  def prefix(s, words, k)
+    s1 = ""
+    if k <= words.length
+      for i in 0...k
+        s1 << words[i]
+      end
+    end
+    return true if s == s1
+    false
+  end
+end
+
+t1 = Test.new
+puts t1.prefix("mohit", ["m", "o", "h", "i", "t", "d", "e", "f"], 5)
+
+#Output = true
