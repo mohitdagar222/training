8fd61d83d765e59c769f8e41cf3dfbb480e9e0a2
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-17 14:35:34 +0530

    update logic of counting how many even digit numbers

--- ruby/ruby_programs/even_array.rb
+++ ruby/ruby_programs/even_array.rb
@@ -3,9 +3,18 @@
 
 class Test
   def even(arr)
-    count = 0
+    digits = []
     for i in 0..arr.length - 1
-      if arr[i] % 2 == 0
+      digit = 0
+      while arr[i] > 0
+        digit += 1
+        arr[i] /= 10
+      end
+      digits.push(digit)
+    end
+    count = 0
+    for i in 0...digits.length
+      if digits[i] % 2 == 0
         count += 1
       end
     end
@@ -14,6 +23,6 @@ class Test
 end
 
 t1 = Test.new
-puts t1.even([2, 1, 3, 4, 5, 6, 7, 8, 9, 24, 67, 88, 97])
+puts t1.even([2, 1, 3, 4, 5, 6, 7, 8, 9, 24, 67, 88, 97, 1002, 1004])
 
-#Output -6
+#Output = 6
