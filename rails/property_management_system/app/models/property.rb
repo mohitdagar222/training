aca50c1dff4f397460984bb80280539d90156332
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-26 13:10:59 +0530

    validates location of property using format

--- rails/property_management_system/app/models/property.rb
+++ rails/property_management_system/app/models/property.rb
@@ -2,4 +2,5 @@ class Property < ApplicationRecord
   validates :name, presence: { message: "can't blank" }
   validates :price, comparison: { greater_than: 50000 }
   validates :location, exclusion: { in: %w(delhi goa), message: "reserved values" }
+  validates :location, format: { with: /\A[a-zA-Z]+\z/, message: "wrong format" }
 end
