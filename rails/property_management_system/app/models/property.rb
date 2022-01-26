7080716ffb5a5da35c998d8f7872d18ba8e2119f
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-26 13:32:09 +0530

    validates location of property using inclusion

--- rails/property_management_system/app/models/property.rb
+++ rails/property_management_system/app/models/property.rb
@@ -3,4 +3,5 @@ class Property < ApplicationRecord
   validates :price, comparison: { greater_than: 50000 }
   validates :location, exclusion: { in: %w(delhi goa), message: "reserved values" }
   validates :location, format: { with: /\A[a-zA-Z]+\z/, message: "wrong format" }
+  validates :location, inclusion: { in: %w(palwal hodal), message: "not in valid location" }
 end
