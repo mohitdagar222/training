efd72a804df55a43e3242ab226ce85f29bc1aa62
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-26 12:56:41 +0530

    validates location of property using exclusion

--- rails/property_management_system/app/models/property.rb
+++ rails/property_management_system/app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
   validates :name, presence: { message: "can't blank" }
   validates :price, comparison: { greater_than: 50000 }
+  validates :location, exclusion: { in: %w(delhi goa), message: "reserved values" }
 end
