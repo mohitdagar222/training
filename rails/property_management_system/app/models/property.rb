cd8c5eb3ce5bd442a6aebf8e4d95ec9f8d69f12f
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-26 13:46:54 +0530

    validates type of property length in 2..6

--- rails/property_management_system/app/models/property.rb
+++ rails/property_management_system/app/models/property.rb
@@ -4,4 +4,5 @@ class Property < ApplicationRecord
   validates :location, exclusion: { in: %w(delhi goa), message: "reserved values" }
   validates :location, format: { with: /\A[a-zA-Z]+\z/, message: "wrong format" }
   validates :location, inclusion: { in: %w(palwal hodal), message: "not in valid location" }
+  validates :ptype, length: { in: 4..10 }
 end
