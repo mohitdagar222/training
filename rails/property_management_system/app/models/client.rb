7d08df7f9b2497e24bd3fb2ccaaaa0c35ed93bde
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-27 11:38:26 +0530

    validates client name should present if specific mobile is entered

--- rails/property_management_system/app/models/client.rb
+++ rails/property_management_system/app/models/client.rb
@@ -1,5 +1,11 @@
 class Client < ApplicationRecord
   validates :mobile, numericality: { only_integer: true }
-  validates :dob, absence: true
+  #validates :dob, absence: true
   validates :mobile, uniqueness: true
+  #validates :name, presence: { strict: true }
+  validates :name, presence: true, if: :mobile_is?
+
+  def mobile_is?
+    mobile == 8930931208
+  end
 end
