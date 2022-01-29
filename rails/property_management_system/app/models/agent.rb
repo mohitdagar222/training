d089e0f8121ec307718436207cc9e30cc3cd68da
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-29 16:00:47 +0530

    add validations to agent model

---
+++ rails/property_management_system/app/models/agent.rb
@@ -0,0 +1,10 @@
+class Agent < ApplicationRecord
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :contact, uniqueness: true
+  validates :contact, length: { is: 10 }
+  validates :contact, numericality: true
+  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+end
