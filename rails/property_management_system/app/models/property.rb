67f4ca2ce4974c5cccdbe5ffa36684c902bf42b7
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-29 21:49:02 +0530

    add has_many and belongs_to association to model property and type

--- rails/property_management_system/app/models/property.rb
+++ rails/property_management_system/app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
-  validates :price, format: { with: /\A[a-zA-Z1-9]+\z/, message: "wrong format" }
+  validates :price, format: { with: /[a-zA-Z1-9]/, message: "wrong format" }
   validates :name, presence: true
-  validates :area, format: { with: /\A[a-zA-Z1-9]+\z/, message: "wrong format" }
+  validates :area, format: { with: /[a-zA-Z1-9]/, message: "wrong format" }
   validates :type_id, numericality: true
   validates :price, presence: true
   validates :location, presence: true
@@ -9,4 +9,6 @@ class Property < ApplicationRecord
   validates :type_id, presence: true
   validates :agent_id, presence: true
   validates :agent_id, numericality: true
+
+  belongs_to :type
 end
--- rails/property_management_system/app/models/type.rb
+++ rails/property_management_system/app/models/type.rb
@@ -1,4 +1,6 @@
 class Type < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
+
+  has_many :properties
 end
