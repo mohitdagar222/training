6c614364aa31e83c08b0ac208cf8d96dd40277e4
mohitdagar222 <mohitdagar222@gmail.com>
2022-01-29 16:02:32 +0530

    add validations to appointment model

--- rails/property_management_system/app/models/appointment.rb
+++ rails/property_management_system/app/models/appointment.rb
@@ -1,2 +1,11 @@
 class Appointment < ApplicationRecord
+  require "date"
+  validates :agent_id, presence: true
+  validates :client_id, presence: true
+  validates :property_id, presence: true
+  validates :date, presence: true
+  validates :client_id, uniqueness: { scope: :date }
+  validates :agent_id, uniqueness: { scope: :date }
+  validates_uniqueness_of :property_id, :scope => [:agent_id, :client_id]
+  validates :date, comparison: { greater_than_or_equal_to: Date.today }
 end
