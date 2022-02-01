d98938cfcd2f2136800b8d03bef104eb77b3d370
mohitdagar222 <mohitdagar222@gmail.com>
2022-02-01 13:07:44 +0530

    add custom validation in appointment table

--- rails/property_management_system/app/models/appointment.rb
+++ rails/property_management_system/app/models/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment < ApplicationRecord
   require "date"
-  validates :agent_id, presence: true
+
   validates :client_id, presence: true
   validates :property_id, presence: true
   validates :date, presence: true
@@ -8,4 +8,16 @@ class Appointment < ApplicationRecord
   validates :agent_id, uniqueness: { scope: :date }
   validates_uniqueness_of :property_id, :scope => [:agent_id, :client_id]
   validates :date, comparison: { greater_than_or_equal_to: Date.today }
+
+  belongs_to :property
+  belongs_to :client
+  belongs_to :agent
+
+  validate :validate
+
+  def validate
+    if !Agent.find(agent_id).properties.ids.include?(property_id)
+      self.errors.add :agent_id, "property does not belongs this agent"
+    end
+  end
 end
