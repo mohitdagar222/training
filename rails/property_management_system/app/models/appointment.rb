745d5bad9eab48acee2498441deeab2717b8e3af
mohitdagar222 <mohitdagar222@gmail.com>
2022-02-02 11:14:52 +0530

    add after_destroy callback in appointment model

--- rails/property_management_system/app/models/appointment.rb
+++ rails/property_management_system/app/models/appointment.rb
@@ -16,8 +16,16 @@ class Appointment < ApplicationRecord
   validate :validate
 
   def validate
-    if !Agent.find(agent_id).properties.ids.include?(property_id)
+    if !Agent.find(self.agent_id).properties.ids.include?(self.property_id)
       self.errors.add :agent_id, "property does not belongs this agent"
     end
+  end
+
+  after_destroy :agent_greet
+
+  private
+
+  def agent_greet
+    puts "Thank you #{self.agent.name} for dealing by our platform"
   end
 end
