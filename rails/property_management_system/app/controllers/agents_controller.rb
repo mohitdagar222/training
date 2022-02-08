fe57cc9a560af24f1e8f54a88f62d4d1f0016125
mohitdagar222 <mohitdagar222@gmail.com>
2022-02-08 12:19:46 +0530

    create controller agents with action show, create and new

--- rails/property_management_system/app/controllers/agents_controller.rb
+++ rails/property_management_system/app/controllers/agents_controller.rb
@@ -1,5 +1,35 @@
 class AgentsController < ApplicationController
-  def info
-    @agent = params[:id]
+  def show
+    @agent = Agent.find(params[:id])
+    if @agent.dob == nil
+      @dob = nil
+    else
+      @dob = @agent.dob.strftime("%d-%m-%Y")
+    end
+  end
+
+  def index
+    @agents = Agent.all
+  end
+
+  def form
+  end
+
+  def create
+    interests = [params[:type1], params[:type2], params[:type3], params[:type4]]
+    interest2 = []
+    interests.each { |interest|
+      if interest == nil
+        next
+      else
+        interest2 << interest
+      end
+    }
+
+    @agent = Agent.new(name: params[:name], email: params[:email], contact: params[:contact], gender: params[:gender], dob: params[:dob], city: params[:city], interests: interest2)
+    @agent.save
+
+    id = Agent.last.id
+    redirect_to "/agent/#{id}"
   end
 end
