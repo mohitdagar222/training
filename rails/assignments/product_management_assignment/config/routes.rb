5e70f1fef15779abbb68432484a67fe57d2c7885
mohitdagar222 <mohitdagar222@gmail.com>
2022-02-11 13:30:53 +0530

    add paths to routes

--- rails/assignments/product_management_assignment/config/routes.rb
+++ rails/assignments/product_management_assignment/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "products/index"
+  root "users#index"
+  get "/login" => "users#index"
+  get "/signup" => "users#signup"
+  post "/users" => "users#create"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/products" => "products#index"
+  get "/products/create" => "products#new"
+  post "/products" => "products#create"
+  get "/product/:id" => "products#edit"
+  delete "/product/:id" => "products#delete"
+  get "/logout" => "products#logout"
+  resources :products
 end
