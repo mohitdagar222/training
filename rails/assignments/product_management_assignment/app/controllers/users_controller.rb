b938ce18adbae9e2e0d7dba929cee2dfd2e43496
mohitdagar222 <mohitdagar222@gmail.com>
2022-02-11 13:25:17 +0530

    generate users controller and create actions for login and signup

---
+++ rails/assignments/product_management_assignment/app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def index
+    @name = params[:name]
+    @password = params[:password]
+    @user = User.find_by(name: @name, password: @password)
+    @id = 0
+    if @user != nil
+      session[:user_id] = @user.id
+      redirect_to "/products"
+    elsif @name != nil
+      @id = 1
+      render "index"
+    end
+  end
+
+  def signup
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:message] = "Your Account Created Successfully"
+      redirect_to "/"
+    else
+      render "signup", status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :dob, :address, :password_confirmation, :terms_of_services)
+  end
+end
