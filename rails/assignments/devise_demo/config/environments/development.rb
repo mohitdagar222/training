7317d660ba03f4d80df5f9f3685b2ec76d7799b2
mohitdagar222 <mohitdagar222@gmail.com>
2022-02-16 16:53:21 +0530

    configure gmail in development file to send mails using action mailer

--- rails/assignments/devise_demo/config/environments/development.rb
+++ rails/assignments/devise_demo/config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -58,6 +58,31 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: "noreplystudybite@gmail.com" }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "google.com",
+    user_name: "noreplystudybite@gmail.com",
+    password: "noreply@1234",
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
