fb7975abea8c0e092764ddb934d001cffd06b6d9
mohitdagar222 <mohitdagar222@gmail.com>
2022-02-19 15:27:23 +0530

    add index, show, destroy, update, create actions to products controller

--- rails/assignments/demo-api-app/app/controllers/api/v1/products_controller.rb
+++ rails/assignments/demo-api-app/app/controllers/api/v1/products_controller.rb
@@ -1,8 +1,36 @@
-class Api::V1::CategoriesController < ActionController::Base
+class Api::V1::ProductsController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   def index
     prod = Product.all
-    render json: { data: prod }
+    render json: { status: "SUCCESS", message: "Loaded successfully", data: prod }, status: :ok
+  end
+
+  def create
+    prod = Product.create(product_params)
+    render json: { status: "SUCCESS", message: "Created successfully", data: prod }, status: :ok
+  end
+
+  def show
+    prod = Product.find(params[:id])
+    render json: { status: "SUCCESS", message: "Fetched successfully", data: prod }, status: :ok
+  end
+
+  def destroy
+    prod = Product.find(params[:id])
+    prod.destroy
+    render json: { status: "SUCCESS", message: "Deleted successfully", data: prod }, status: :ok
+  end
+
+  def update
+    prod = Product.find(params[:id])
+    prod.update(product_params)
+    render json: { status: "SUCCESS", message: "Updated successfully", data: prod }, status: :ok
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:id, :name, :price)
   end
 end
